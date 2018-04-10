88d12bf3164aeb82a22d3e5f3181e6840b48ba1f
Mathieu DUCROT <mathieu.ducrot@pia-production.fr>
2018-04-10 15:40:27 +0200

    Add show / edit / delete action on list view

---
+++ src/Admin/Extension/ActionExtension.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Smart\SonataBundle\Admin\Extension;
+
+use Sonata\AdminBundle\Admin\AbstractAdminExtension;
+use Sonata\AdminBundle\Admin\FieldDescriptionInterface;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+
+/**
+ * @author Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
+ */
+class ActionExtension extends AbstractAdminExtension
+{
+    /**
+     * @var string
+     */
+    private $action;
+
+    /**
+     * @param string $action
+     */
+    public function __construct($action)
+    {
+        $this->action = $action;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureListFields(ListMapper $list)
+    {
+        if (!$list->getAdmin()->isGranted(strtoupper($this->action))) {
+            return;
+        }
+
+        if (!$list->has('_action')) {
+            $this->createActionField($list);
+
+            return;
+        }
+
+        $field = $list->get('_action');
+
+        if ('actions' === $field->getType()) {
+            $this->alterActionField($field);
+        }
+    }
+
+    /**
+     * @param FieldDescriptionInterface $field
+     */
+    private function alterActionField(FieldDescriptionInterface $field)
+    {
+        $field->setOption(
+            'actions',
+            array_merge(
+                $field->getOption('actions'),
+                [
+                    $this->action => [
+                        'template' => sprintf('@SmartSonata/action/%s.html.twig', $this->action)
+                    ]
+                ]
+            )
+        );
+    }
+
+    /**
+     * @param ListMapper $list
+     */
+    private function createActionField(ListMapper $list)
+    {
+        $list->add('_action', 'actions', [
+            'actions' => [
+                $this->action => [
+                    'template' => sprintf('@SmartSonata/action/%s.html.twig', $this->action)
+                ]
+            ]
+        ]);
+    }
+}
