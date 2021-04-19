79396f6145828d20d853b9e6b4fd4d52ef822b6a
Mathieu Ducrot <mathieu.ducrot@smartbooster.io>
2021-04-19 16:44:37 +0200

    #15 Set alice_data_fixtures no_purge mode and disable other drivers than orm

--- src/DependencyInjection/SmartSonataExtension.php
+++ src/DependencyInjection/SmartSonataExtension.php
@@ -2,6 +2,7 @@
 
 namespace Smart\SonataBundle\DependencyInjection;
 
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -11,7 +12,7 @@ use Symfony\Component\Config\FileLocator;
 /**
  * @author Mathieu Ducrot <mathieu.ducrot@pia-production.fr>
  */
-class SmartSonataExtension extends Extension
+class SmartSonataExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * @param array<object> $configs
