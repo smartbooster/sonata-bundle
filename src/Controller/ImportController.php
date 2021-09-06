c64fccb66374634b38f0ac3b13daf24632dbbceb
Mathieu Ducrot <mathieu.ducrot@smartbooster.io>
2021-09-06 18:14:42 +0200

    Complete Import action submit with ETL DoctrineInsertUpdateLoader

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -3,6 +3,8 @@
 namespace Smart\SonataBundle\Controller;
 
 use Smart\EtlBundle\Exception\Utils\ArrayUtils\MultiArrayHeaderConsistencyException;
+use Smart\EtlBundle\Loader\DoctrineInsertUpdateLoader;
+use Smart\EtlBundle\Utils\StringUtils;
 use Smart\SonataBundle\Admin\ImportableAdminInterface;
 use Smart\SonataBundle\Form\Type\ImportType;
 use Smart\EtlBundle\Utils\ArrayUtils;
@@ -13,6 +15,9 @@ use Sonata\AdminBundle\Request\AdminFetcherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
@@ -46,26 +51,49 @@ class ImportController extends AbstractController
 
         $form = $this->createForm(ImportType::class);
         $form->handleRequest($request);
-        if ($form->isSubmitted() and $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             try {
+                $rawData = $form->getData()['raw_data'];
                 $dataToImport = ArrayUtils::getMultiArrayFromString(
-                    $form->getData()['raw_data'],
+                    $rawData,
                     $admin->getImportHeader(),
                     $admin->getImportOptions()
                 );
 
+                /** @var \Doctrine\ORM\EntityManagerInterface $em */
+                $em = $this->getDoctrine()->getManager();
+                $entityClass = $admin->getClass();
                 // @phpstan-ignore-next-line
                 if ($form->get('import_preview')->isClicked()) {
-                    $importPreviewData = (new DiffGenerator($this->getDoctrine()->getManager()))->generateDiffs(
-                        $admin->getClass(),
+                    $importPreviewData = (new DiffGenerator($em))->generateDiffs(
+                        $entityClass,
                         $dataToImport
                     );
                 // @phpstan-ignore-next-line
                 } elseif ($form->get('import')->isClicked()) {
-                    // @todo real import
-                    $this->addFlash('sonata_flash_success', $this->trans('import.label_success', [
-                        // @todo retour $importNbs
-                    ]));
+                    $normalizer = new ObjectNormalizer();
+                    $serializer = new Serializer([$normalizer]);
+
+                    $dataToImport = array_map(function ($data) use ($serializer, $entityClass) {
+                        return $serializer->denormalize($data, $entityClass);
+                    }, $dataToImport);
+
+                    $loader = new DoctrineInsertUpdateLoader($em);
+                    $identifier = $admin->getImportHeader()[0];
+                    $accessor = PropertyAccess::createPropertyAccessor();
+                    $loader->addEntityToProcess(
+                        $entityClass,
+                        function ($o) use ($identifier, $accessor) {
+                            return $accessor->getValue($o, $identifier);
+                        },
+                        $identifier,
+                        $admin->getImportHeader()
+                    );
+                    $loader->load($dataToImport);
+
+                    $loaderLogs = $loader->getLogs()[$entityClass];
+                    $loaderLogs['nb_skipped'] = StringUtils::countRows($rawData) - $loaderLogs['nb_created'] - $loaderLogs['nb_updated'];
+                    $this->addFlash('sonata_flash_success', $this->trans('import.label_success', $loaderLogs));
 
                     return $this->redirect($admin->generateUrl('list'));
                 } // else 'cancel_import' the form display again with the data kept in the fields
